02be7211ae5fd3cbe7db61db465c57d449bf9ea2
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-06-10 02:10:21 +0200

    Update urls.py

--- fictional_band/polls/urls.py
+++ fictional_band/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+"""Roots the view 'details' to the url.py file
+
+"""
+
 app_name = "polls"
 urlpatterns = [
     path('<int:question_id>/detail/', views.detail, name='detail'),
