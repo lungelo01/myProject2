93f8e608c8e5c60e2e81f2dba46690d61115c8eb
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-06-10 02:14:43 +0200

    Update urls.py

--- fictional_band/the_blues_brothers/urls.py
+++ fictional_band/the_blues_brothers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+"""roots 'view' and 'home' to the urls.py file
 
+"""
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
